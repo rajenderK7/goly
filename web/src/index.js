80c3bc72678985658b7524bfe513c49a8295fbb1
Rajender <rajenderkatkuri123@gmail.com>
2023-12-25 22:02:01 +0530

    Create frontend

--- web/src/index.js
+++ web/src/index.js
@@ -1,31 +1,48 @@
 const longURLField = document.getElementById("long-url")
 const shortURLField = document.getElementById("short-url")
 const shortenBtn = document.getElementById("shorten-btn")
+const URI = "https://goly-backend.onrender.com"
+const createURL = URI + "/create"
 
 const getShortURL = async (longURL) => {
     let data
     try {
-        const res = await fetch("https://goly-backend.onrender.com/create", {
+        const res = await fetch(createURL, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Access-Control-Allow-Origin": "*",
             },
-            // mode: "no-cors",
+            mode: "cors",
             body: JSON.stringify({ "long_url": longURL })
         })
-        data = res.json()
+        data = await res.json()
     } catch (e) {
-        console.log(e);
+        alert(e)
+    } finally {
+        return data
     }
-    console.log(data)
+}
+
+const isValidReq = () => {
+    return longURLField.value !== ""
 }
 
 shortenBtn.addEventListener("click", async (event) => {
     event.preventDefault()
     shortenBtn.disabled = true
 
-    const longURL = longURLField.value
-    await getShortURL(longURL)
+    if (!isValidReq()) {
+        shortenBtn.disabled = false
+        alert("Enter valid URL")
+        return
+    }
+
+    const data = await getShortURL(longURLField.value)
+    if (!data) {
+        shortenBtn.disabled = false
+        return
+    }
+
+    shortURLField.value = URI + "/" + data["short_url"]
     shortenBtn.disabled = false
 })
